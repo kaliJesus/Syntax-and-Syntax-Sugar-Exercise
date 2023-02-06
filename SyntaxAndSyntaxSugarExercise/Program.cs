9f47fe06fd0cd552636cd15cdcb85363d294b17c
Kalkidan Shibeshi <kkalitemesgen@gmail.com>
2023-02-06 15:14:22 -0500

    new commit

--- SyntaxAndSyntaxSugarExercise/Program.cs
+++ SyntaxAndSyntaxSugarExercise/Program.cs
@@ -4,7 +4,21 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var answer = 4;
+            var response = "";
+            if (answer < 9) 
+            {
+                response = $"{answer} is less than nine";
+            }
+            else
+            {
+                response = $"{answer} is greater than or equal to 9";
+            }
+            Console.WriteLine(response);
+
+            var response1 = (answer < 9)? $"{answer} is less than nine" : $"{answer} is greater than nine";
+            Console.WriteLine(response1);
+       
         }
     }
 }
